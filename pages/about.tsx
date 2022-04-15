bfa2e8ca9d3ff36617110bbd34ef0948409c3057
mrHyde90 <a00516283@gmail.com>
2022-04-14 21:08:54 -0500

    next first app

---
+++ pages/about.tsx
@@ -0,0 +1,31 @@
+
+import Link from 'next/link';
+import { DarkLayout } from '../components/layouts/DarkLayout';
+import { MainLayout } from '../components/layouts/MainLayout';
+import { ReactElement } from 'react';
+
+
+export default function AboutPage() {
+  return (
+    <>
+      <h1 className="title">
+          ir a <Link href="/">Home</Link>
+        </h1>
+
+        <p className="description">
+          Get started by editing{' '}
+          <code className="code">pages/about.jsx</code>
+        </p>
+    </>
+  )
+}
+
+AboutPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MainLayout>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
+    </MainLayout>
+  );
+}
